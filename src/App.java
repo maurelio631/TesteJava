c052dfc8cf6687d0b5932b74cfccf5b917345b58
Marco <marco.silva631@outlook.com>
2021-10-07 20:44:37 -0300

    Criação do primeiro programinha de calculadora apenas de soma

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner entrada = new Scanner(System.in);
+        int num1;
+        int num2;
+        int soma;
+        System.out.println("Seja bem-vindo à sua calculadora");
+        System.out.println("Insira o primeiro número da soma: ");
+        num1 = entrada.nextInt();
+        System.out.println("Insira o segundo número da soma: ");
+        num2 = entrada.nextInt();
+        soma = num1 + num2;
+        System.out.println("O resultado da soma é: "+soma);
     }
 }
